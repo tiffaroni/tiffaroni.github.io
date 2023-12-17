904909358a9fc53f3ef4d730b05f7aff24b031d0
tiffany <tiffanylin757@gmail.com>
2023-12-16 20:14:56 -0600

    adding info

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,3 +41,28 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
+
+
+const collapsibleBtns = document.querySelectorAll('.collapsible-icon');
+const collapsibleContents = document.querySelectorAll('.content');
+
+collapsibleBtns.forEach((btn, btnIdx) => {
+    btn.addEventListener('click', () =>{
+        let icon = btn.firstElementChild;
+        if(icon.classList.contains('fa-chevron-down')){
+            icon.className = 'fa fa-chevron-up';
+            collapsibleContents.forEach((content, contentIdx) => {
+                if(btnIdx == contentIdx){
+                    content.classList.add('content-show');
+                }
+            })
+        } else if(icon.classList.contains('fa-chevron-up')){
+            icon.className = 'fa fa-chevron-down';
+            collapsibleContents.forEach((content, contentIdx) => {
+                if(btnIdx == contentIdx){
+                    content.classList.remove('content-show');
+                }
+            });
+        }
+    })
+});
